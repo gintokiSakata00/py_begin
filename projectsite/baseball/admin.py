6c34f16c09eb7917a88cfcfd417400cd706fcb0b
gintokiSakata00 <gillbertpadon01@gmail.com>
2022-11-29 00:49:35 +0800

    all done

--- projectsite/baseball/admin.py
+++ projectsite/baseball/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.admin import display
-from .models import Position, Person, Club, Play
+from .models import Position, Person, Club, Play, Match
+
+
+# Register your models here.
 
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
@@ -8,17 +11,19 @@ class PositionAdmin(admin.ModelAdmin):
     search_fields = ("description",)
 
 @admin.register(Person)
-class PositionAdmin(admin.ModelAdmin):
-    list_display = ("firstname","lastname","height","weight")
-    search_fields = ("firstname","lastname","height","weight")
-
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("lastname","firstname","height","weight")
+    search_fields = ("lastname",)
 
 @admin.register(Club)
-class PositionAdmin(admin.ModelAdmin):
-    list_display = ("name","coach","dorm_latitude","dorm_longhitude")
-    search_fields = ("name","coach","dorm_latitude","dorm_longhitude")
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ("name","coach",)
+    search_fields = ("name",)
 
 @admin.register(Play)
-class PositionAdmin(admin.ModelAdmin):
-    list_display = ("player","team","string_no","pos")
-    list_display = ("player","team","string_no","pos")
+class PlayAdmin(admin.ModelAdmin):
+    list_display = ("player","team","string_no","isActive",)
+
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ("team1","score_t1","team2","score_t2","winner",)
